f25ff4b5fc86fd689e5ecc3afbb65f1c38aaa993
Benjamín <themidnightguyyy@gmail.com>
2026-03-09 03:17:43 -0300

    Update files & .gitkeep /logs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 #import
 from fastapi import FastAPI
-
 #import model.py/schema.py
 from app.schema import UserImput, UserOutput
 from app.model import prediccion
+#logging
+import logging
 
+#Configuramos logger
+logging.basicConfig(
+    filename="log/api_taller.log",
+    level=logging.INFO,
+    format= '%(asctime)s | %(message)s'
+    )
+
+logger = logging.getLogger(__name__)
+
+#FastAPI
 
 app = FastAPI(title= 'Predicción Taller')
 
@@ -16,9 +27,19 @@ def home():
     })
 
 #Prediccion - (/predict) - POST
+#Se creara un log tras cada consulta ejecutada por los usuarios
 @app.post('/predict', response_model= UserOutput)
 def predecir_semanas(claim: UserImput):
     resultado = prediccion(claim.model_dump())
+
+    logger.info(
+        f"claim_id={claim.claim_id} | "
+        f"marca={claim.marca_vehiculo} | "
+        f"tipo_poliza={claim.tipo_poliza} | "
+        f"taller={claim.taller} | "
+        f"prediccion={resultado}"
+    )
+
     return UserOutput(
         claim_id=claim.claim_id,
         prediccion_semanas=resultado
